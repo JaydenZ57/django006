1d74c081415cd0e82f78c70da9083c77ac928ff4
JaydenZ57 <zzjayden57@gmail.com>
2022-04-02 18:42:25 -0400

    django006

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tum#mc_0zx^h0l!sw0d31ng#gsf=s649yod-ra=97q_gk!org#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wordcount',
+    'jobs',
 ]
 
 MIDDLEWARE = [
